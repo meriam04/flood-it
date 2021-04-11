4ecd2b0a6e1531f914231afd5c8de56caa84d6e4
meriam04 <meriam_fourati@yahoo.com>
2021-04-11 12:58:56 -0400

    Update main_keyboard.c

--- main_keyboard.c
+++ main_keyboard.c
@@ -57,6 +57,7 @@
 #define NUM_ROWS 10
 #define NUM_COLS 10
 #define NUM_COLOURS 6
+#define OUTLINE 2
 
 #define FALSE 0
 #define TRUE 1
@@ -77,6 +78,11 @@ typedef struct {
     short int colour;
 } CellInfo;
 
+typedef struct {
+    int row;
+    int col;
+} SelectedCell;
+
 
 // function definitions
 void set_A9_IRQ_stack(void);
@@ -85,7 +91,7 @@ void config_interrupt(int N, int CPU_target);
 void config_KEYs(void);
 void config_PS2();
 void enable_A9_interrupts(void);
-//void disable_A9_interrupts(void);	//if use, disable then enable interrupts (prob unneccesary)
+//void disable_A9_interrupts(void);    //if use, disable then enable interrupts (prob unneccesary)
 
 void __attribute__((interrupt)) __cs3_isr_irq(void);
 void __attribute__((interrupt)) __cs3_reset(void);      //where are these called?
@@ -117,6 +123,8 @@ void initialize_board();
 void free_board();
 void draw_board();
 void reinitialize_board();
+void erase_selected_cell();
+void draw_selected_cell();
 
 
 
@@ -131,6 +139,8 @@ int y_cursor = 0;
 short int colours[] = {YELLOW, GREEN, BLUE, CYAN, MAGENTA, GREY, PINK, ORANGE, WHITE, RED};
 int num_turns = 25;
 int won_game = FALSE;
+
+SelectedCell selected_cell; // making the selected cell a global variable
 
 /* key_dir and pattern are written by interrupt service routines; we have to
 * declare these as volatile to avoid the compiler caching their values in
@@ -150,9 +160,9 @@ int main(void)
     config_PS2();
     enable_A9_interrupts(); // enable interrupts on proc
    
-	//volatile int * PS2_ptr = (int *)PS2_BASE;
-   //*(PS2_ptr) = 0xFF;	//reset the mouse
-	
+    //volatile int * PS2_ptr = (int *)PS2_BASE;
+   //*(PS2_ptr) = 0xFF;    //reset the mouse
+    
     //int num_turns = 25;
     //int won_game = FALSE;
   
@@ -166,6 +176,10 @@ int main(void)
     volatile int * pixel_ctrl_ptr = (int *)PIXEL_BUF_CTRL_BASE;
     
     initialize_board();
+    
+    // initializing selected cell to be the bottom right
+    selected_cell.row = rows - 2;
+    selected_cell.col = cols - 2;
 
     /* set front pixel buffer to start of FPGA On-chip memory */
     *(pixel_ctrl_ptr + 1) = FPGA_ONCHIP_BASE; // first store the address in the
@@ -189,10 +203,11 @@ int main(void)
     */
 
     draw_board();
+    draw_selected_cell();
     
     
-	draw_box(x_cursor % RESOLUTION_X, y_cursor % RESOLUTION_Y, 3, WHITE);
-	
+    draw_box(x_cursor % RESOLUTION_X, y_cursor % RESOLUTION_Y, 3, WHITE);
+    
     while ( !won_game && (num_turns > 0))
     {
         /* Erase any boxes and lines that were drawn in the last iteration */
@@ -206,13 +221,13 @@ int main(void)
         int erase_colour = board[row][col].colour;*/
         short int erase_colour = colour_from_pos(x_pos, y_pos);
     
-	    //erase cursor
+        //erase cursor
         draw_box(x_pos, y_pos, 3, erase_colour);
         
         display_hex(0,0, num_turns);
         // short int colour = colours[iteration % NUM_COLOURS];
        
-	    //MOUSE IMPLEMENTATION////////////////////////////////////////////////////////
+        //MOUSE IMPLEMENTATION////////////////////////////////////////////////////////
         /*PS2_data = *(PS2_ptr); // read the Data register in the PS/2 port
         RVALID = PS2_data & 0x8000; // extract the RVALID field
 
@@ -221,7 +236,7 @@ int main(void)
             byte1 = byte2;
             byte2 = byte3;
             byte3 = PS2_data & 0xFF;
-		
+        
             x_cursor += byte2;
             y_cursor += byte3;
             
@@ -233,9 +248,9 @@ int main(void)
                     apply_colour(clicked_colour);
                     num_turns--;
                 }
-				// iteration++;
-				// printf("xpos: %d, ypos: %d\n",x_cursor, y_cursor);
-	  		}
+                // iteration++;
+                // printf("xpos: %d, ypos: %d\n",x_cursor, y_cursor);
+              }
             if ((byte2 == (char)0xAA) && (byte3 == (char)0x00)){
                 // mouse inserted; initialize sending of data
                 *(PS2_ptr) = 0xF4;
@@ -245,7 +260,7 @@ int main(void)
         // check if won
         won_game = check_won_game();
         
-	    //draw cursor
+        //draw cursor
         draw_box(x_cursor % RESOLUTION_X, y_cursor % RESOLUTION_Y, 3, WHITE);
         //printf("iteration: %d, colour: %x\n",iteration, colour);
         
@@ -254,7 +269,7 @@ int main(void)
         // pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
         
         
-	}
+    }
     
     // exited while loop means either won or lost game
     display_win_lose_hex(won_game);
@@ -333,6 +348,11 @@ void reinitialize_board(){
     won_game = FALSE;
     initialize_board();
     draw_board();
+    
+    selected_cell.row = rows - 2;
+    selected_cell.col = cols - 2;
+    
+    draw_selected_cell();
 }
 
 void apply_colour(short int colour){
@@ -435,6 +455,19 @@ void set_level(int level){
     return;
 }
 
+void draw_selected_cell(){
+    // drawing selected box with a white outline of thickness 2
+    CellInfo current_cell = board[selected_cell.row][selected_cell.col];
+    // draw outline first
+    draw_box(current_cell.x_pos, current_cell.y_pos, size, WHITE);
+    // then draw actual box
+    draw_box(current_cell.x_pos + OUTLINE, current_cell.y_pos + OUTLINE, size - 2*OUTLINE, current_cell.colour);
+}
+
+void erase_selected_cell(){
+    CellInfo current_cell = board[selected_cell.row][selected_cell.col];
+    draw_box(current_cell.x_pos, current_cell.y_pos, size, current_cell.colour);
+}
 void draw_box(int x, int y, int size, short int color){
     
     for (int i = 0; i < size; ++i){
@@ -443,6 +476,7 @@ void draw_box(int x, int y, int size, short int color){
         }
     }
 }
+
 
 // using Bresenham's algorithm
 void draw_line(int x0, int y0, int x1, int y1, short int color){
@@ -595,13 +629,13 @@ void display_win_lose_hex(int won_game){
     
     // create an array with the letters for "passed"
     unsigned char passed_table[] = {
-        0b0000000001110011 /*P*/, 0b0000000001110111  /*A*/, 0b0000000001101101 /*S*/, 
-	    0b0000000001101101 /*S*/, 0b0000000001111001  /*E*/, 0b0000000001011110 /*d*/
+        0b0000000001110011 /*P*/, 0b0000000001110111  /*A*/, 0b0000000001101101 /*S*/,
+        0b0000000001101101 /*S*/, 0b0000000001111001  /*E*/, 0b0000000001011110 /*d*/
     };
     
     unsigned char failed_table[] = {
-        0b0000000001110001  /*F*/, 0b0000000001110111  /*A*/, 0b0000000000110000 /*I*/, 
-	    0b0000000000111000 /*L*/, 0b0000000001111001  /*E*/, 0b0000000001011110 /*d*/
+        0b0000000001110001  /*F*/, 0b0000000001110111  /*A*/, 0b0000000000110000 /*I*/,
+        0b0000000000111000 /*L*/, 0b0000000001111001  /*E*/, 0b0000000001011110 /*d*/
     };
     
     unsigned char hex_segs[] = {0, 0, 0, 0, 0, 0, 0, 0};
@@ -643,8 +677,8 @@ void config_KEYs()
 
 void config_PS2()
 {
-	volatile int* PS2_ptr = (int*) PS2_BASE;
-	*(PS2_ptr + 1) = 0x1;	//enable interrupts by setting RE bit =1
+    volatile int* PS2_ptr = (int*) PS2_BASE;
+    *(PS2_ptr + 1) = 0x1;    //enable interrupts by setting RE bit =1
 }
 
 /*Initialize the banked stack pointer register for IRQ mode*/
@@ -684,12 +718,12 @@ void config_GIC(void)
     int address; // used to calculate register addresses
     
     /* configure the FPGA PS2 and KEYs interrupts */
-    //*((int *)0xFFFED848) = 0x00000100;	//to specify CPU target (ICDIPTRn) - one byte per interrupt
-    //*((int *)0xFFFED84C) = 0x01000000;	//3rd byte for PS2: index=n%4=3 (has to be byte 0,1,2 or 3)
-    //*((int *)0xFFFED108) = 0x00008200;	//for key AND ps2 set-enables (ICDISERn) - enable specific interrupts to be forwarded to the CPU interface - one BIT per interrupt
+    //*((int *)0xFFFED848) = 0x00000100;    //to specify CPU target (ICDIPTRn) - one byte per interrupt
+    //*((int *)0xFFFED84C) = 0x01000000;    //3rd byte for PS2: index=n%4=3 (has to be byte 0,1,2 or 3)
+    //*((int *)0xFFFED108) = 0x00008200;    //for key AND ps2 set-enables (ICDISERn) - enable specific interrupts to be forwarded to the CPU interface - one BIT per interrupt
     config_interrupt(73,1);
-	config_interrupt (79,1);
-	
+    config_interrupt (79,1);
+    
     // Set Interrupt Priority Mask Register (ICCPMR). Enable interrupts of all
     //priorities
     address = MPCORE_GIC_CPUIF + ICCPMR;
@@ -702,7 +736,7 @@ void config_GIC(void)
     
     // Configure the Distributor Control Register (ICDDCR) to send pending
     // interrupts to CPUs
-    address = MPCORE_GIC_DIST + ICDDCR;  
+    address = MPCORE_GIC_DIST + ICDDCR;
     *((int *)address) = 1;
 }
 
@@ -728,7 +762,7 @@ void config_interrupt(int N, int CPU_target) {
     appropriate byte */
     *(char *)address = (char)CPU_target;
 }
-		    
+            
 //IRQ EXCEPTION HANDLER
 // Define the IRQ exception handler
 void __attribute__((interrupt)) __cs3_isr_irq(void)
@@ -740,7 +774,7 @@ void __attribute__((interrupt)) __cs3_isr_irq(void)
     if (int_ID == KEYS_IRQ) // check if interrupt is from the KEYS
         pushbutton_ISR();
     else if (int_ID == PS2_IRQ) // check if interrupt is from the PS2 port, add sw interrupt ID later
-        keyboard_ISR();	
+        keyboard_ISR();
     else
         while (1)
             ; // if unexpected, then stay here
@@ -751,7 +785,7 @@ void __attribute__((interrupt)) __cs3_isr_irq(void)
     return;
 }
 
-// Define the remaining exception handlers (writes to 
+// Define the remaining exception handlers (writes to
 void __attribute__((interrupt)) __cs3_reset(void)
 {
     while (1)
@@ -781,7 +815,7 @@ void __attribute__((interrupt)) __cs3_isr_fiq(void)
 {
     while (1)
         ;
-}           
+}
             
             
 
@@ -794,19 +828,19 @@ void pushbutton_ISR(void)
 {
     printf("in pushbutton interrupt\n");
     volatile int * KEY_ptr = (int *)KEY_BASE;
-	volatile int * LEDR_ptr = (int*)LEDR_BASE;
+    volatile int * LEDR_ptr = (int*)LEDR_BASE;
     volatile int * SW_ptr = (int*)SW_BASE;
     
     int press;
     press = *(KEY_ptr + 3); // read the pushbutton interrupt register
     *(KEY_ptr + 3) = press; // Clear the interrupt
-	printf("key %d was pressed\n",press-1);
-	
-	//if(press&0x1)//key0 is pressed
-	//else if (press&0x2)//key1
-	//else if (press&0x4)//key2
-	//else//key3
-	
+    printf("key %d was pressed\n",press-1);
+    
+    //if(press&0x1)//key0 is pressed
+    //else if (press&0x2)//key1
+    //else if (press&0x4)//key2
+    //else//key3
+    
     
     // printf("in interrupt\n");
     // change this part to read from switch and call the level setting function
@@ -823,81 +857,112 @@ void pushbutton_ISR(void)
     return;
 }
 
-void keyboard_ISR(void)	//interrupt triggered w ANY mvmt: clear it every time, and execute ONLY if click (byte1: 0000101, others are moot) (need to store all movement to see total x,y?)
+void keyboard_ISR(void)    //interrupt triggered w ANY mvmt: clear it every time, and execute ONLY if click (byte1: 0000101, others are moot) (need to store all movement to see total x,y?)
 {
-	volatile int * PS2_ptr = (int *)PS2_BASE;
-	volatile int * LEDR_ptr= (int*)LEDR_BASE;
-	int PS2_data, PS2_control, RAVAIL, RVALID;
+    volatile int * PS2_ptr = (int *)PS2_BASE;
+    volatile int * LEDR_ptr= (int*)LEDR_BASE;
+    int PS2_data, PS2_control, RAVAIL, RVALID;
     unsigned char byte1 = 0, byte2 = 0;
-	//have to read this first!!! first read below::::
-	PS2_data = *(PS2_ptr);	//reading from ps2data (lower 8 bits are Data) automatically decrements ravail/num on stack=>next byte now in PS2_data
-	RAVAIL = PS2_data & 0xFFFF0000;
-    RVALID = PS2_data & 0x8000;	
-	
-	printf("interruptedkey\n data %d\n",PS2_data);
-	*(LEDR_ptr) = key_dir;
-	key_dir ^=1;
-	
-	int flag = *(PS2_ptr +1) & 0x100;
-	if (!flag)
-		printf("interrupt flag cleared after oneread\n");
-	
-    if(RVALID && (RAVAIL==0)){ //RVALID means data is available, ravail=0 means data has stopped being sent (press is over)
-    	byte1 = PS2_data & 0xFF;
-		
-		if(byte1==(char)0xF0){//break code (key is done being pressed, read next byte -> break code sends 2 bytes
-			
-			PS2_data = *(PS2_ptr);/////second read (after check for break)
-			RVALID = PS2_data & 0x8000;
-			RAVAIL = PS2_data & 0xFFFF0000; //top 16 BITS!!!
-			
-			flag = *(PS2_ptr +1) & 0x100;
-			if (!flag)
-				printf("interrupt flag cleared after read 2\n char %x\n", byte1);
-		
-			if (RVALID && (RAVAIL==0)){
-				byte2=PS2_data & 0xFF;
-			   
-				if (byte2==(char)0x1C){
-					printf("move left\n");
-					return;
-				}
-				else if (byte2==(char)0x23){
-					printf("move right\n");
-					return;
-				}	
-				else if (byte2==(char)0x1D){
-					printf("move up\n");
-					return;
-				}	
-				else if (byte2==(char)0x1B){
-					printf("move down\n");
-					return;
-				}	
-				else if (byte2==(char)0x5A){
-					printf("select box\n");
-					return;
-				}	
-				else {
-					printf("invalid key press\n");
-					return;
-				}	
-			}
-		}
-	}
-	
-    flag = *(PS2_ptr +1) & 0x100;
-	if (!flag)
-		printf("interrupt flag cleared from none interrupt\n");
+    //have to read this first!!! first read below::::
+    PS2_data = *(PS2_ptr);    //reading from ps2data (lower 8 bits are Data) automatically decrements ravail/num on stack=>next byte now in PS2_data
+    RAVAIL = PS2_data & 0xFFFF0000;
+    RVALID = PS2_data & 0x8000;
     
-	/*do we need to detect edge of screen?
- 	if ((byte2 == (char)0xAA) && (byte3 == (char)0x00)){//chck if initialization of mouse is completed
-		// mouse inserted; initialize sending of data //aka mouse hit left corner?
-		*(PS2_ptr) = 0xF4;	//command to initialize data reporting, enables data reporting and resets its movement counters (need for edge of screen??)
-	}*/
-	//write to lower 8 bits of data reg to send commands => check CE to see if error recieving it
-	
-	
+    printf("interruptedkey\n data %d\n",PS2_data);
+    *(LEDR_ptr) = key_dir;
+    key_dir ^=1;
+    
+    int flag = *(PS2_ptr +1) & 0x100;
+    if (!flag)
+        printf("interrupt flag cleared after oneread\n");
+    
+    if(RVALID && (RAVAIL==0)){ //RVALID means data is available, ravail=0 means data has stopped being sent (press is over)
+        byte1 = PS2_data & 0xFF;
+        
+        if(byte1==(char)0xF0){//break code (key is done being pressed, read next byte -> break code sends 2 bytes
+            
+            PS2_data = *(PS2_ptr);/////second read (after check for break)
+            RVALID = PS2_data & 0x8000;
+            RAVAIL = PS2_data & 0xFFFF0000; //top 16 BITS!!!
+            
+            flag = *(PS2_ptr +1) & 0x100;
+            if (!flag)
+                printf("interrupt flag cleared after read 2\n char %x\n", byte1);
+        
+            if (RVALID && (RAVAIL==0)){
+                byte2=PS2_data & 0xFF;
+               
+                if (byte2==(char)0x1C){
+                    printf("move left\n");
+                    // check if selected cell can move left first
+                    // then decrememnt column if can
+                    if (selected_cell.col > 1){
+                        // redraw current selected cell first then draw new one too
+                        erase_selected_cell();
+                        selected_cell.col--;
+                        draw_selected_cell();
+                    }
+                    return;
+                }
+                else if (byte2==(char)0x23){
+                    printf("move right\n");
+                    if (selected_cell.col < (cols - 2)){
+                        erase_selected_cell();
+                        selected_cell.col++;
+                        draw_selected_cell();
+                    }
+                    
+                    return;
+                }
+                else if (byte2==(char)0x1D){
+                    printf("move up\n");
+                    if (selected_cell.row > 1){ // check here not sure about this
+                        erase_selected_cell();
+                        selected_cell.row--;
+                        draw_selected_cell();
+                    }
+                    return;
+                }
+                else if (byte2==(char)0x1B){
+                    printf("move down\n");
+                    if (selected_cell.row < (rows - 2)){ // check here not sure about this
+                        erase_selected_cell();
+                        selected_cell.row++;
+                        draw_selected_cell();
+                    }
+                    return;
+                }
+                else if (byte2==(char)0x5A){
+                    printf("select box\n");
+                    short int clicked_colour = colour_from_pos(x_cursor % RESOLUTION_X, y_cursor % RESOLUTION_Y);
+                    // change colour to selected colour
+                    if ((clicked_colour != BLACK) && clicked_colour != board[1][1].colour){
+                        apply_colour(clicked_colour);
+                        num_turns--;
+                        //draw_board();
+                    }
+                    return;
+                }
+                else {
+                    printf("invalid key press\n");
+                    return;
+                }
+            }
+        }
+    }
+    
+    flag = *(PS2_ptr +1) & 0x100;
+    if (!flag)
+        printf("interrupt flag cleared from none interrupt\n");
+    
+    /*do we need to detect edge of screen?
+     if ((byte2 == (char)0xAA) && (byte3 == (char)0x00)){//chck if initialization of mouse is completed
+        // mouse inserted; initialize sending of data //aka mouse hit left corner?
+        *(PS2_ptr) = 0xF4;    //command to initialize data reporting, enables data reporting and resets its movement counters (need for edge of screen??)
+    }*/
+    //write to lower 8 bits of data reg to send commands => check CE to see if error recieving it
+    
+    
     return;
 }
 
